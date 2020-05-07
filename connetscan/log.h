ef7d60a1b513dcb98b0e86d428e7aa4d3b62e1a4
Dies1rae <unluck.bastard@gmail.com>
2020-05-07 18:48:22 +0300

    //comments

--- connetscan/log.h
+++ connetscan/log.h
@@ -15,8 +15,12 @@ public:
 		this->logdump.push_back(L);
 	}
 	~logg() {}
+
+	//add string into log class
 	void add_log_string(string L);
+	//simple cout all from class
 	void cout_log_string();
+	//get all log(strings) in vector to somewhere 
 	vector <string> get_all_log_string();
 };
 
--- connetscan/main.cpp
+++ connetscan/main.cpp
@@ -10,12 +10,19 @@ using namespace std;
 
 
 int main() {
+	//init log
 	logg* dumplog = new logg("Starting");
+	//init scan
 	scan* test = new scan();
+	//set log in scan
 	test->set_init_log(dumplog);
+	//set option
 	test->set_scan_option();
+	//start
 	test->start_scan();
+	//get res from log
 	dumplog->cout_log_string();
+	//get res last from scan
 	cout << endl << endl << "TOTAL RESULT: ";
 	cout << test->get_scan_result() << endl;
 
